7cd3d2e737d34445f827b42a8fceefeac09c50f3
Jessica <jessica.weeber@gmail.com>
2016-06-30 14:06:03 -0700

    i made a lot of stuff and forgot to commit.. so here is some jquery and js for initializing a new game and players

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,63 @@
-function TicTacToe() {
+function Player(id, symbol) {
+  this.id = id
+  this.symbol = symbol
+}
 
+function TicTacToe() {
+  
 }
 
 TicTacToe.prototype = {
+  startGame: function () {
+    this.player1 = new Player(1, "🐱")
+    this.player2 = new Player(2, "🐶")
+    this.turn = this.player1
+  },
   
+  takeTurn: function (player) {
+    // if (this.player1) {
+      square.addClass("active")
+      button.text("💩")
+    // } 
+  }
 }
+
+$(document).on('ready', function() {
+  
+  var game = new TicTacToe()
+  game.startGame()
+
+  if (game.turn === game.player1) {
+    game.turn = game.player2;
+  } else {
+    game.turn = game.player1;
+  }
+
+  var buttons = $('button')
+  buttons.on('mousedown', function(event)  {  
+    event.preventDefault()
+    var button = $(this)
+
+    var squareID = $(this).data('square')
+    var square = $('.' + squareID)
+
+  if (game.turn === game.player1) {
+    console.log(game.player1)
+    square.addClass("active")
+    button.text(player1.symbol)
+  }
+  })
+})
+
+// What constitutes a turn?
+// determine player turn
+// select a space
+  // space available?
+  // space advantageous?
+// claim space using player identifier
+// check win/loss/draw state
+  // what is a win
+  // what is a draw
+  // what is a loss
+  // communicate outcome
+// continue play
